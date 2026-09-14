b8112d48aaf01439333da773b31a122022faade5
hamidjavdani <77245983+hamidjavdani@users.noreply.github.com>
2026-09-14 15:46:00 +0330

    Add MSB API key header setting

--- Configurations/MsbSettings.cs
+++ Configurations/MsbSettings.cs
@@ -4,6 +4,7 @@ public class MsbSettings
 {
     public string BaseUrl { get; set; } = "http://pmsbgw.shahr-bank.ir";
     public string InquiryEndpoint { get; set; } = "/interagency/document-verification-inquiry/G2GInquery";
+    public string ApiKeyHeaderName { get; set; } = "X-MSB-Api-Key";
     public string ApiKey { get; set; } = string.Empty;
     public string RuleId { get; set; } = "mhrne7iv";
 }
