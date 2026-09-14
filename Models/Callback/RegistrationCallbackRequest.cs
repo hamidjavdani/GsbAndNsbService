c5de453afeecea2f8992e5cd8d899dabd700bd36
hamidjavdani <77245983+hamidjavdani@users.noreply.github.com>
2026-09-14 15:52:03 +0330

    Restore legacy GSB callback request model

--- Models/Callback/RegistrationCallbackRequest.cs
+++ Models/Callback/RegistrationCallbackRequest.cs
@@ -4,14 +4,8 @@ namespace GSB.Test.Api.Models.Callback;
 
 public class RegistrationCallbackRequest
 {
-    [JsonPropertyName("organId")]
-    public string OrganId { get; set; } = string.Empty;
-
     [JsonPropertyName("code")]
     public int Code { get; set; }
-
-    [JsonPropertyName("owTrakingCode")]
-    public string OwTrakingCode { get; set; } = string.Empty;
 
     [JsonPropertyName("data")]
     public RegistrationCallbackData? Data { get; set; }
