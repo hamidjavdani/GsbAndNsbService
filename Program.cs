c9394c79aab33544f3942d7eb462a1f311e0c305
hamidjavdani <77245983+hamidjavdani@users.noreply.github.com>
2026-09-14 15:44:37 +0330

    Register MSB services

--- Program.cs
+++ Program.cs
@@ -1,49 +1,38 @@
-//using GSB.Test.Api.Configurations;
 using GSB.Test.Api.Configurations;
 using GSB.Test.Api.Data;
 using GSB.Test.Api.Services;
 using Microsoft.EntityFrameworkCore;
 
-
-
 var builder = WebApplication.CreateBuilder(args);
-
-
-// Add services to the container.
-
 
 builder.Services.Configure<GsbSettings>(
     builder.Configuration.GetSection("GSB"));
 
-//builder.Services.AddHttpClient();
+builder.Services.Configure<MsbSettings>(
+    builder.Configuration.GetSection("MSB"));
 
 builder.Services.AddHttpClient("GSB", client =>
 {
     client.Timeout = TimeSpan.FromSeconds(60);
-
     client.DefaultRequestHeaders.Accept.Clear();
-
     client.DefaultRequestHeaders.Accept.Add(
         new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 });
 
-
-
-//builder.Services.AddScoped<GsbService>();
+builder.Services.AddHttpClient("MSB", client =>
+{
+    client.Timeout = TimeSpan.FromSeconds(60);
+    client.DefaultRequestHeaders.Accept.Clear();
+    client.DefaultRequestHeaders.Accept.Add(
+        new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+});
 
 builder.Services.AddScoped<IGsbService, GsbService>();
-
+builder.Services.AddScoped<IMsbService, MsbService>();
 builder.Services.AddScoped<IRegistrationCallbackService, RegistrationCallbackService>();
-
-builder.Services.AddScoped<IRegistrationCallbackService, RegistrationCallbackService>();
-
 
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
-
-//builder.Services.AddSwaggerGen();
-
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
@@ -51,15 +40,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
         builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
