765cb9142412d8adcdf89c4e30c174ad52d9e7e4
hamidjavdani <77245983+hamidjavdani@users.noreply.github.com>
2026-09-14 15:46:09 +0330

    Align callback root model with MSB contract

--- Models/Callback/RegistrationCallbackRequest.cs
+++ Models/Callback/RegistrationCallbackRequest.cs
@@ -1,26 +1,21 @@
-﻿using System.Text.Json.Serialization;
+using System.Text.Json.Serialization;
 
 namespace GSB.Test.Api.Models.Callback;
 
 public class RegistrationCallbackRequest
 {
-    /// <summary>
-    /// کد وضعیت Callback
-    /// 200 = موفق
-    /// 201 = خطا
-    /// </summary>
+    [JsonPropertyName("organId")]
+    public string OrganId { get; set; } = string.Empty;
+
     [JsonPropertyName("code")]
     public int Code { get; set; }
 
-    /// <summary>
-    /// اطلاعات دریافتی از پنجره واحد
-    /// </summary>
+    [JsonPropertyName("owTrakingCode")]
+    public string OwTrakingCode { get; set; } = string.Empty;
+
     [JsonPropertyName("data")]
     public RegistrationCallbackData? Data { get; set; }
 
-    /// <summary>
-    /// اطلاعات خطا
-    /// </summary>
     [JsonPropertyName("error")]
     public RegistrationCallbackError? Error { get; set; }
 }
